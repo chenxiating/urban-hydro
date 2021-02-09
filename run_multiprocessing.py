7363a3808915fc90acf1b6c16282ed81fac3f0f9
Xiating <xiating.chen.du@gmail.com>
2021-02-09 14:56:47 -0600

    updated mp

--- run_multiprocessing.py
+++ run_multiprocessing.py
@@ -3,21 +3,21 @@ import simulation_hydro_network
 import time
 import numpy as np
 
-def main(nodes_num, process_core_name):
-    soil_moisture_list = np.linspace(0, 1, 20)
-    mean_rainfall_set = np.linspace(0, 5, 10)
-    for soil_moisture in soil_moisture_list:
-        for mean_rainfall in mean_rainfall_set:
-            simulation_hydro_network.main(nodes_num, process_core_name, soil_moisture, mean_rainfall)
+def main(nodes_num, process_core_name, soil_moisture, mean_rainfall):
+    simulation_hydro_network.main(nodes_num, process_core_name, soil_moisture, mean_rainfall)
 
 start = time.perf_counter()
 
 if __name__ == '__main__':
+    soil_moisture_list = np.linspace(0, 1, 20)
+    mean_rainfall_set = np.linspace(0, 5, 10)
     processes = []
     for k in range(10):
-        p = multiprocessing.Process(target = main, args = (int(100), k))
-        p.start()
-        processes.append(p)
+        for soil_moisture in soil_moisture_list:
+            for mean_rainfall in mean_rainfall_set:
+                p = multiprocessing.Process(target = main, args = (int(100), k, soil_moisture, mean_rainfall))
+                p.start()
+                processes.append(p)
 
     for process in processes:
         # print('Running process: ', process)
